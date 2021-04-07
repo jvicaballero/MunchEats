41ae4f0661ecfb9225d3645e73cf0cd5f84a58bf
Alan Eappen <eappenalan123@gmail.com>
2021-04-07 13:11:39 -0400

    designed the login and signup pages

--- src/Login.js
+++ src/Login.js
@@ -1,49 +1,62 @@
-import React, {useCallback, useContext} from "react";
-import {withRouter, Redirect} from "react-router";
+import React, { useCallback, useContext } from "react";
+import { withRouter, Redirect } from "react-router";
 import app from "./base.js";
-import {AuthContext} from "./Auth"
+import "./styles/login.css";
+import { AuthContext } from "./Auth";
 // import Signup from "./Signup";
 
-const Login = ({history}) => {
+const Login = ({ history }) => {
     const handleLogin = useCallback(
         async event => {
             event.preventDefault();
-            const {email, password} = event.target.elements;
+            const { email, password } = event.target.elements;
             try {
                 await app
-                .auth()
-                .signInWithEmailAndPassword(email.value, password.value);
+                    .auth()
+                    .signInWithEmailAndPassword(email.value, password.value);
                 history.push("/");
 
-            }catch(error){
+            } catch (error) {
                 alert(error);
             }
         }, [history]
     );
 
-    const {currentUser} = useContext(AuthContext);
+    const { currentUser } = useContext(AuthContext);
 
-    if(currentUser) {
+    if (currentUser) {
         return <Redirect to="/" />
     }
 
-    return(
+    return (
         <div>
-            <h1>Login</h1>
-            <form onSubmit={handleLogin}>
-                <label>Email
-                    <input name="email" type="email" placeholder="Email"/>
-                </label>
+            <div className="block2">
+            </div>
 
-                 <label>Password
-                    <input name="password" type="password" placeholder="Password"/>
-                 </label>
-                 <button type="submit">Login</button>
-            </form>
+            <div className="block1 d-flex justify-content-center align-items-center">
+                <h1> Munch Eats</h1>
+            </div>
 
-            <button onClick={() => history.push('/signup')}>Create an Account</button>
+            <div className="logo"></div>
+
+
+            <div className="loginContainer p-5">
+                <form className="d-flex flex-column" onSubmit={handleLogin}>
+                    <div>
+                        <h5>Email</h5>
+                        <input type="email" className="form-control textField" name="email" />
+                        <h5 className="mt-4">Password</h5>
+                        <input type="password" class="form-control textField" name="password" />
+                    </div>
+
+                    <div className="d-flex flex-column mt-5 text-center">
+                        <button style={{ fontSize: "19px" }} className="textField bg-info" type="submit">Login</button>
+                        <p className="mt-2">Dont have an Account?  <a style={{ cursor: "pointer", color: "blue" }} onClick={() => history.push('/signup')}>Sign Up</a> </p>
+                    </div>
+                </form>
+            </div>
         </div>
-        
+
     )
 
 
--- src/Signup.js
+++ src/Signup.js
@@ -1,36 +1,50 @@
-import React, {useCallback} from "react";
-import {withRouter} from "react-router";
+import React, { useCallback } from "react";
+import { withRouter } from "react-router";
 import app from "./base";
+import "./styles/login.css"
 
-const Signup = ({history}) => {
+const Signup = ({ history }) => {
     const handleSignup = useCallback(async event => {
         event.preventDefault();
-        const { email, password} = event.target.elements;
+        const { email, password } = event.target.elements;
         try {
             await app
-            .auth()
-            .createUserWithEmailAndPassword(email.value, password.value);
+                .auth()
+                .createUserWithEmailAndPassword(email.value, password.value);
             history.push("/");
 
-        }catch (error) {
+        } catch (error) {
             alert(error);
         }
     }, [history]);
 
-    return(
+    return (
         <div>
-            <h1>Signup</h1>
-            <form onSubmit={handleSignup}>
-                <label>Email
-                    <input name="email" type="email" placeholder="Email"/>
-                </label>
+            <div className="block2Dasher">
+            </div>
 
-                 <label>Password
-                    <input name="password" type="password" placeholder="Password"/>
-                 </label>
-                 <button type="submit">Sign up</button>
-            </form>
+            <div className="block1 d-flex justify-content-center align-items-center">
+                <h1> Munch Eats</h1>
+            </div>
 
+            <div className="logo"></div>
+
+
+            <div className="loginContainer p-5">
+                <form className="d-flex flex-column" onSubmit={handleSignup}>
+                    <div>
+                        <h5>Email</h5>
+                        <input type="email" className="form-control textField" name="email" />
+                        <h5 className="mt-4">Password</h5>
+                        <input type="password" class="form-control textField" name="password" />
+                    </div>
+
+                    <div className="d-flex flex-column mt-5 text-center">
+                        <button style={{ fontSize: "19px" }} className="textField bg-info" type="submit">Signup</button>
+                        <p className="mt-2">Have an Account?  <a style={{ cursor: "pointer", color: "blue" }} onClick={() => history.push('/login')}>Login</a> </p>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
